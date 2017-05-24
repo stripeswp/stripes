4125355b9c3711ca1e663ab4ea8e353516b7ea35
Bryan Hadaway <bhadaway@gmail.com>
2017-05-24 07:34:56 -0600

    Updates

--- about.php
+++ about.php
@@ -14,7 +14,7 @@ function stripes_options_do_page()
 <?php echo "<h1>" . sprintf( __( '%s', 'stripes' ), $current_theme->Name ) . "</h1>"; ?>
 <p><?php printf( __( 'Thank you for choosing Stripes.', 'stripes' ) ); ?></p>
 <p><?php printf( __( 'You can customize Stripes under <em>Appearance</em> > <em>Customize</em>.', 'stripes' ) ); ?></p>
-<p><?php printf( __( 'If at any point you need assistance with Stripes, please visit our <a href="https://github.com/stripeswp/stripes/issues" target="_blank">support page</a>.', 'stripes' ) ); ?></p>
+<p><?php printf( __( 'If at any point you need assistance with Stripes, please visit the <a href="https://github.com/stripeswp/stripes/issues" target="_blank">support page</a>.', 'stripes' ) ); ?></p>
 </div>
 <?php
 }
---
+++ readme.txt
@@ -0,0 +1,8 @@
+Stripes
+https://stripeswp.com/
+
+Please post support questions and feature requests here:
+
+https://github.com/stripeswp/stripes/issues
+
+Thank you.
